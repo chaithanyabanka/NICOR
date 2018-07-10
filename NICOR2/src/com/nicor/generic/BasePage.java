0d2ddb96cc6423fc31bb09aa034667d1fb1ca6a3
Admin <Admin@192.168.0.8>
2018-07-11 00:00:02 +0530

    all generics

---
+++ NICOR2/src/com/nicor/generic/BasePage.java
@@ -0,0 +1,52 @@
+package com.nicor.generic;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
+
+public class BasePage 
+{
+	public WebDriver driver;
+	public void Basepage(WebDriver driver)
+	{
+		this.driver=driver;
+	}
+	
+	//to verify the title
+	public void verifyTitle(String eTitle)
+	{
+		WebDriverWait wait=new WebDriverWait(driver,10);
+		try
+		{
+			wait.until(ExpectedConditions.titleIs(eTitle));
+			Reporter.log("Title is matching :"+eTitle,true);
+			
+		}
+		catch(Exception e)
+		{
+			Reporter.log("Title is not matching : Actual title is :"+driver.getTitle(),true);
+			Assert.fail();
+		}
+	}
+	
+	//to verify the element
+	public void verifyElement(WebElement element)
+	{
+		WebDriverWait wait=new WebDriverWait(driver,10);
+		try
+		{
+			wait.until(ExpectedConditions.visibilityOf(element));
+			Reporter.log("Element is present :",true);
+			
+		}
+		catch(Exception e)
+		{
+			Reporter.log("Element is not present :",true);
+			Assert.fail();
+		}
+	}
+
+}
