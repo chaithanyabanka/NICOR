2b2caa88e36a1b40c3abf607e7253dd35d9e6224
Admin <Admin@192.168.0.8>
2018-07-10 23:52:26 +0530

    selenium project <10/07/2018>
    "autoconstant" interface will contains all constant variable annd it is
    implemented by baseTest class.
    
    "BaseTest" class contains the test methods like precondition() and
    postcondition() to open the browser and enter the url and close the
    browser.
    
    "BasePage" class contains the methods verifyTtle() and verifyElement().
    
    "Generic Utils" class contains the methods getScreenShot() and Select
    class methos like slectbyindex() , selectbytext(), selectbytext().

---
+++ NICOR/src/com/nicor/generic/AutoConstant.java
@@ -0,0 +1,11 @@
+package com.nicor.generic;
+
+interface AutoConstant 
+{	
+	String chrome_key="webdriver.chrome.driver"; 
+	String chrome_value="./drivers/chromedriver.exe";
+	String gecko_key="webdriver.gecko.driver";
+	String gecko_value="./drivers/geckodriver.exe";
+	String file_path="./data/input.xlsx";
+
+}
